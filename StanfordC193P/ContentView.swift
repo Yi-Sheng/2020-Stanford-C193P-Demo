19d2f5f1196563ba5ea5433a41fd56c58cde1b51
Eason Hsu <icecomic5566@livemail.tw>
2020-05-29 16:21:19 +0800

    Initial Commit

---
+++ StanfordC193P/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StanfordC193P
+//
+//  Created by Eason Hsu S on 2020/5/29.
+//  Copyright © 2020 Eason Hsu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
